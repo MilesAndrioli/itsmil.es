024e457de436ceda57ed56a875a74df65cd38d15
Miles <git@itsmil.es>
2024-05-25 03:52:35 +0200

    perfected postcss.config

--- webpack/postcss.config.js
+++ webpack/postcss.config.js
@@ -1,5 +1,27 @@
-module.exports = () => {
+/**
+ * PostCSS configuration file as configured under cssRules.use['postcss-loader']
+ * in development.js and production.js
+ *
+ * @docs https://postcss.org/docs/
+ * @plugins https://postcss.org/docs/postcss-plugins
+ */
+module.exports = (projectOptions) => {
+    let plugins = [];
+
+    if (projectOptions.projectCss.use === "sass") {
+        plugins.push(require("autoprefixer"));
+    } else {
+        plugins.push(
+            require("autoprefixer")
+            // require("postcss-import-ext-glob"),
+            // require("postcss-import"),
+            // require("postcss-nested-ancestors"),
+            // require("postcss-nested"),
+            // require("postcss-advanced-variables")
+        );
+    }
+
     return {
-        plugins: [require("autoprefixer")],
+        plugins: plugins,
     };
 };
