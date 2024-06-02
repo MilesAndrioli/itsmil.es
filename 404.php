1bd4f13ae94bc1ae0c4c92b03a4c950ab11e573f
Miles <git@itsmil.es>
2024-06-02 05:29:40 +0200

    minimal 404 and index

---
+++ 404.php
@@ -0,0 +1,11 @@
+<?php
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+get_header(); ?>
+
+<h1>404</h1>
+
+<?php get_footer();
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+get_header(); ?>
+
+<h1>INDEX</h1>
+
+<?php get_footer();
