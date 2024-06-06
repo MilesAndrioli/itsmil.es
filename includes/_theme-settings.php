0597a11577279a21ffc5a94d53bb39cb7bd31870
Miles <git@itsmil.es>
2024-06-06 20:47:46 +0200

    perfected theme-settings

--- includes/_theme-settings.php
+++ includes/_theme-settings.php
@@ -5,6 +5,9 @@ if (!defined('ABSPATH')) {
 }
 
 if (!function_exists('theme_settings')) {
+    /**
+     * Configures theme-specific settings.
+     */
     function theme_settings()
     {
         /**
@@ -13,6 +16,16 @@ if (!function_exists('theme_settings')) {
          * @link https://developer.wordpress.org/block-editor/how-to-guides/themes/theme-support/#editor-styles
          */
         add_theme_support('editor-styles');
+
+        /**
+         * This function allows to register menus and expose them in the 
+         * WordPress admin panel for easy management.
+         */
+        register_nav_menus(
+            array(
+                'app-header-navigation' => esc_html__('App Header Navigation', 'itsmiles'),
+            )
+        );
     }
     add_action('after_setup_theme', 'theme_settings');
 }
