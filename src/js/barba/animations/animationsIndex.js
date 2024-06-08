469dc9b72f00ca2765ab1e7bf15910d3331a8875
Miles <git@itsmil.es>
2024-06-08 05:05:27 +0200

    animation Indexes

---
+++ src/js/barba/animations/animationsIndex.js
@@ -0,0 +1,10 @@
+const context = require.context(".", false, /\.js$/);
+
+const barbaAnimations = context
+    .keys()
+    .filter((file) => file !== "./animationsIndex.js")
+    .reduce((acc, file) => {
+        return { ...acc, ...context(file).default };
+    }, {});
+
+export default barbaAnimations;
