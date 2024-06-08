d707a9d645c1a2daf6d0770033f38c8fbf826f43
Miles <git@itsmil.es>
2024-06-08 05:13:37 +0200

    clean gsap zoom

---
+++ src/js/gsap/animations/zoom.js
@@ -0,0 +1,10 @@
+export default {
+    zoom: {
+        origin: { scale: 0.8 },
+        target: { scale: 1 },
+    },
+    "zoom-fade": {
+        origin: { scale: 0.8, opacity: 0 },
+        target: { scale: 1, opacity: 1 },
+    },
+};
