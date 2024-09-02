75b032f41d925eba62dff36304608f7535c0fdea
Miles <miles.andrioli@gmail.com>
2024-09-02 12:58:47 +0200

    ++

---
+++ parts/app-footer.php
@@ -0,0 +1,17 @@
+<?php
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+?>
+
+<footer id="app-footer" unveil-wrapper>
+    <div class="container">
+        <div unveil-target>
+            <h2>FOOTER</h2>
+            <h2>FOOTER</h2>
+            <h2>FOOTER</h2>
+        </div>
+    </div>
+</footer>
