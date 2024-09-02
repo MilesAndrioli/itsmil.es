fa7df118020a03365411c7bb6f3df97d32c4dfd5
Miles <miles.andrioli@gmail.com>
2024-09-02 16:38:11 +0200

    ++

--- 404.php
+++ 404.php
@@ -6,6 +6,19 @@ if (!defined('ABSPATH')) {
 
 get_header(); ?>
 
-<h1>404</h1>
+<section>
+    <div class="container">
+        <h1>404</h1>
+        <h1>404</h1>
+        <h1>404</h1>
+        <h1>404</h1>
+        <h1>404</h1>
+        <h1>404</h1>
+        <h1>404</h1>
+        <h1>404</h1>
+        <h1>404</h1>
+        <h1>404</h1>
+    </div>
+</section>
 
 <?php get_footer();
