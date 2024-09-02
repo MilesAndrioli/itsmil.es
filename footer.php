feec45e33a55052962ce09a81a10ce1079891b60
Miles <miles.andrioli@gmail.com>
2024-09-02 11:48:10 +0200

    ++

---
+++ footer.php
@@ -0,0 +1,20 @@
+<?php
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+?>
+
+                </main> <!-- #appMain -->
+
+                <!-- App Footer -->
+                <?php get_template_part('parts/app-footer'); ?>
+
+            </div> <!-- #smooth-content -->
+        </div> <!-- #smooth-wrapper -->
+    </div> <!-- #appWrapper -->
+
+<?php wp_footer(); ?>
+</body>
+</html>
