cb87f87dd196bb836632aa9313de9af516d180e8
Miles <miles.andrioli@gmail.com>
2024-09-02 15:33:40 +0200

    ++

--- src/js/utilities/exitLoader.js
+++ src/js/utilities/exitLoader.js
@@ -4,11 +4,8 @@ const consoleLog = createDebugLogger(debug);
 
 /**
  * Recommended to execute in the window.load native JS event.
- * Adds a class to the body to signal that the page has loaded and is ready to
- * be shown without layout shifts.
- *
- * @param {string} [target='#app-loader'] - CSS selector for the loader element.
- * Defaults to '#app-loader'.
+ * Adds a class to the body to signal that the page has loaded and is ready to be shown without layout shifts.
+ * @param {string} [target='#app-loader'] - CSS selector for the loader element. Defaults to '#app-loader'.
  */
 export default function exitLoader(target = "#app-loader") {
     const loader = document.querySelector(target);
