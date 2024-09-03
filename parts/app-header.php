7d17c9ea0fb5d08da8c205ec53d393f1b0856f94
Miles <miles.andrioli@gmail.com>
2024-09-03 12:20:09 +0200

    ++

---
+++ parts/app-header.php
@@ -0,0 +1,39 @@
+<?php
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+?>
+
+<header id="app-header" class="position-fixed z-1 w-100 py-3">
+    <div class="container">
+        <div class="d-flex align-items-center justify-content-between gap-5">
+
+            <!-- Menu -->
+            <?php wp_nav_menu([
+                'theme_location'    => 'left-header-navigation',
+                'container'         => 'nav',
+                'menu_class'        => 'list-unstyled d-flex gap-3',
+            ]); ?>
+
+            <!-- Logo -->
+            <?php if (has_custom_logo()) : ?>
+                <?php the_custom_logo(); ?>
+            <?php else : ?>
+                <a href="<?php echo esc_url(home_url()); ?>" class="d-grid">
+                    <span class="h4"><?php bloginfo('name'); ?></span>
+                    <small><?php bloginfo('description'); ?></small>
+                </a>
+            <?php endif; ?>
+
+            <!-- Menu -->
+            <?php wp_nav_menu([
+                'theme_location'    => 'right-header-navigation',
+                'container'         => 'nav',
+                'menu_class'        => 'list-unstyled d-flex gap-3',
+            ]); ?>
+            
+        </div>
+    </div>
+</header>
