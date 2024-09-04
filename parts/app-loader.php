bb3a0a932ce716a6b96bb2386feddf47ad46592a
Miles <miles.andrioli@gmail.com>
2024-09-04 13:18:49 +0200

    ++

--- parts/app-loader.php
+++ parts/app-loader.php
@@ -1,1 +1,27 @@
-<div id="app-loader"></div>
+<?php
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+if (has_custom_logo()) {
+    $logo_id = get_theme_mod('custom_logo');
+    $logo_url = wp_get_attachment_image_src($logo_id, 'full')[0];
+}
+
+?>
+
+<div id="app-loader">
+    <img 
+        class="custom-logo app-loader__image-bg"
+        width="250"
+        src="<?php echo esc_url($logo_url); ?>"
+        alt="<?php bloginfo('name'); ?>"
+    />
+    <img 
+        class="custom-logo app-loader__image-filler"
+        width="250"
+        src="<?php echo esc_url($logo_url); ?>"
+        alt="<?php bloginfo('name'); ?>"
+    />
+</div>
