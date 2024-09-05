e21b10500662ebf5dd8bc7c824041657fe763f7a
Miles <miles.andrioli@gmail.com>
2024-09-05 16:56:48 +0200

    ++

--- includes/_global-settings.php
+++ includes/_global-settings.php
@@ -8,3 +8,6 @@ if (!defined('ABSPATH')) {
 add_action('get_header', function() {
     remove_action('wp_head', '_admin_bar_bump_cb');
 });
+
+// Disable native WordPress image optimizations
+add_filter('big_image_size_threshold', '__return_false');
