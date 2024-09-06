c7b5376067377979702ec92cd0038b1db4bf2e0b
Miles <git@itsmil.es>
2024-09-06 23:19:54 +0200

    ++

--- parts/app-loader.php
+++ parts/app-loader.php
@@ -4,24 +4,25 @@ if (!defined('ABSPATH')) {
     exit;
 }
 
-if (has_custom_logo()) {
-    $logo_id = get_theme_mod('custom_logo');
-    $logo_url = wp_get_attachment_image_src($logo_id, 'full')[0];
-}
-
 ?>
 
 <div id="app-loader">
-    <img 
-        class="custom-logo app-loader__image-bg"
-        width="250"
-        src="<?php echo esc_url($logo_url); ?>"
-        alt="<?php bloginfo('name'); ?>"
-    />
-    <img 
-        class="custom-logo app-loader__image-filler"
-        width="250"
-        src="<?php echo esc_url($logo_url); ?>"
-        alt="<?php bloginfo('name'); ?>"
-    />
+    <?php if (has_custom_logo()) : ?>
+        <?php
+        $logo_id = get_theme_mod('custom_logo');
+        $logo_url = wp_get_attachment_image_src($logo_id, 'full')[0];
+        ?>
+        <img
+            class="custom-logo app-loader__image-bg"
+            width="250"
+            src="<?php echo esc_url($logo_url); ?>"
+            alt="<?php bloginfo('name'); ?>" />
+        <img
+            class="custom-logo app-loader__image-filler"
+            width="250"
+            src="<?php echo esc_url($logo_url); ?>"
+            alt="<?php bloginfo('name'); ?>" />
+    <?php else : ?>
+        <?php bloginfo('name'); ?>
+    <?php endif; ?>
 </div>
