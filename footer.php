96465ad9ba64ae24e22de5e4d6deb83cca572de1
Miles <git@itsmil.es>
2024-09-06 23:38:49 +0200

    ++

--- footer.php
+++ footer.php
@@ -6,15 +6,16 @@ if (!defined('ABSPATH')) {
 
 ?>
 
-                </main> <!-- #appMain -->
+</main> <!-- #appMain -->
 
-                <!-- App Footer -->
-                <?php get_template_part('parts/app-footer'); ?>
+<!-- App Footer -->
+<?php get_template_part('parts/app-footer'); ?>
 
-            </div> <!-- #smooth-content -->
-        </div> <!-- #smooth-wrapper -->
-    </div> <!-- #appWrapper -->
+</div> <!-- #smooth-content -->
+</div> <!-- #smooth-wrapper -->
+</div> <!-- #appWrapper -->
 
 <?php wp_footer(); ?>
 </body>
+
 </html>
