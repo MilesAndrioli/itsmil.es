0a7e4678f494afec9e5db6200ae3baaf4f863e12
Miles <git@itsmil.es>
2024-09-06 23:33:45 +0200

    ++

--- includes/_cleaning.php
+++ includes/_cleaning.php
@@ -10,11 +10,8 @@ remove_action('wp_enqueue_scripts', 'wp_enqueue_global_styles');
 // Remove imposed WordPress emoji JS file
 remove_action('wp_head', 'print_emoji_detection_script', 7);
 
-if (!function_exists('dequeue_garbage')) {
-    function dequeue_garbage()
-    {
-        // Dequeue imposed Gutenberg CSS file
-        wp_dequeue_style('wp-block-library');
-    }
-    add_action('wp_enqueue_scripts', 'dequeue_garbage');
-}
+// Remove unnecessary WordPress imposed files
+add_action('wp_enqueue_scripts', function () {
+    // Gutenberg CSS file
+    wp_dequeue_style('wp-block-library');
+});
