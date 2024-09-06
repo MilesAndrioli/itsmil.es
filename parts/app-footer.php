7a3e29dbcc492d9ac66a587f61467aa3cd4f4361
Miles <git@itsmil.es>
2024-09-06 22:26:52 +0200

    ++

--- parts/app-footer.php
+++ parts/app-footer.php
@@ -8,8 +8,8 @@ if (!defined('ABSPATH')) {
 
 <footer id="app-footer" class="bg-onyx border-top py-8" unveil-wrapper>
     <div class="container" aos="split-up-clip-down" split-type="chars" aos-parent unveil-target>
-        <h1 aos-child>FOOTER</h1>
-        <h1 aos-child>FOOTER</h1>
-        <h1 aos-child>FOOTER</h1>
+        <div class="h1" aos-child>FOOTER</div>
+        <div class="h1" aos-child>FOOTER</div>
+        <div class="h1" aos-child>FOOTER</div>
     </div>
 </footer>
