cb895461d0462aa8fd55d33cc3471459e750255d
Miles <git@itsmil.es>
2024-09-07 02:37:20 +0200

    ++

--- src/js/gsap/animations/scale.js
+++ src/js/gsap/animations/scale.js
@@ -7,4 +7,12 @@ export default {
         origin: { scale: 0.8, opacity: 0 },
         target: { scale: 1, opacity: 1 },
     },
+    "scale-out": {
+        origin: { scale: 1.2 },
+        target: { scale: 1 },
+    },
+    "scale-out-fade-in": {
+        origin: { opacity: 0, scale: 1.2 },
+        target: { opacity: 1, scale: 1 },
+    },
 };
