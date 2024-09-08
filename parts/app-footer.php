aab377704896d2719449427eb7dd8430b38fac21
Miles <git@itsmil.es>
2024-09-08 21:18:13 +0200

    ++

--- parts/app-footer.php
+++ parts/app-footer.php
@@ -6,7 +6,7 @@ if (!defined('ABSPATH')) {
 
 ?>
 
-<footer id="app-footer" class="bg-onyx border-top py-8" unveil-wrapper>
+<footer id="app-footer" class="min-vh-50 d-flex align-items-center bg-onyx border-top py-8" unveil-wrapper>
     <div class="container" aos="split-up-clip-down" aos-split-type="chars" aos-parent unveil-target>
         <div class="h1" aos-child>FOOTER</div>
         <div class="h1" aos-child>FOOTER</div>
