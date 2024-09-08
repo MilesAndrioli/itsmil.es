674c7cf31566955d65601420e659900993e1e0a8
Miles <git@itsmil.es>
2024-09-08 06:51:55 +0200

    ++

--- parts/app-header.php
+++ parts/app-header.php
@@ -8,30 +8,36 @@ if (!defined('ABSPATH')) {
 
 <header id="app-header" class="position-fixed z-8 w-100 border-bottom py-3">
     <div class="container">
-        <div class="d-flex align-items-center justify-content-between gap-5">
+        <div class="row row-cols-3 align-items-center">
 
             <!-- Menu -->
-            <?php wp_nav_menu([
-                'theme_location'    => 'left-header-navigation',
-                'container'         => 'nav',
-                'menu_class'        => 'list-unstyled d-flex gap-3',
-            ]); ?>
+            <div>
+                <?php wp_nav_menu([
+                    'theme_location'    => 'left-header-navigation',
+                    'container'         => 'nav',
+                    'menu_class'        => 'list-unstyled d-flex gap-3',
+                ]); ?>
+            </div>
 
             <!-- Logo -->
-            <?php if (has_custom_logo()) : ?>
-                <?php the_custom_logo(); ?>
-            <?php else : ?>
-                <a href="<?php echo esc_url(home_url()); ?>">
-                    <?php bloginfo('name'); ?>
-                </a>
-            <?php endif; ?>
+            <div class="d-flex justify-content-center">
+                <?php if (has_custom_logo()) : ?>
+                    <?php the_custom_logo(); ?>
+                <?php else : ?>
+                    <a href="<?php echo esc_url(home_url()); ?>">
+                        <?php bloginfo('name'); ?>
+                    </a>
+                <?php endif; ?>
+            </div>
 
             <!-- Menu -->
-            <?php wp_nav_menu([
-                'theme_location'    => 'right-header-navigation',
-                'container'         => 'nav',
-                'menu_class'        => 'list-unstyled d-flex gap-3',
-            ]); ?>
+            <div class="d-flex justify-content-end">
+                <?php wp_nav_menu([
+                    'theme_location'    => 'right-header-navigation',
+                    'container'         => 'nav',
+                    'menu_class'        => 'list-unstyled d-flex gap-3',
+                ]); ?>
+            </div>
 
         </div>
     </div>
