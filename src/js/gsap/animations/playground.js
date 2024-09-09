8ec0bed47627e19b105f940af44712a3694bbf60
Miles <git@itsmil.es>
2024-09-09 21:58:35 +0200

    experimental aos animations

---
+++ src/js/gsap/animations/playground.js
@@ -0,0 +1,10 @@
+export default {
+    colorize: {
+        origin: { filter: "grayscale(100%)" },
+        target: { filter: "grayscale(0%)" },
+    },
+    focus: {
+        origin: { filter: "blur(20px)", scale: 1.4 },
+        target: { filter: "blur(0px)", scale: 1 },
+    },
+};
