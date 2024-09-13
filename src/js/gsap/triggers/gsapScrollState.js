4c5d2b7f85a917ffe5ba70a26a26b2dba9859524
Miles <git@itsmil.es>
2024-09-13 06:40:55 +0200

    scrollState

--- src/js/gsap/triggers/gsapScrollState.js
+++ src/js/gsap/triggers/gsapScrollState.js
@@ -1,24 +1,46 @@
+const debug = false;
+
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
 
 export default function initGsapScrollState() {
-    const body = document.body;
+    const body = document.querySelector("#app-body");
 
     // Detect Scroll Milestones
+    // ScrollTrigger.create({
+    //     trigger: body,
+    //     start: `${innerHeight / 1.333333333333333 + APP_HEADER_HEIGHT} top`,
+    //     toggleClass: "HAS-REACHED--STEP-0",
+    //     markers: debug,
+    // });
+
+    // ScrollTrigger.create({
+    //     trigger: body,
+    //     start: `${innerHeight} top+=${APP_HEADER_HEIGHT}`,
+    //     toggleClass: "HAS-REACHED--STEP-2",
+    //     markers: debug,
+    // });
+
     ScrollTrigger.create({
-        trigger: body,
-        start: `${innerHeight / 1.333333333333333 + APP_HEADER_HEIGHT} top`,
-        toggleClass: "HAS-REACHED--STEP-1",
-        // markers: true,
+        trigger: "#app-main > section:first-of-type",
+        endTrigger: body,
+        start: "bottom top",
+        toggleClass: {
+            targets: body,
+            className: "HAS-REACHED--STEP-1",
+        },
+        markers: true,
     });
 
     ScrollTrigger.create({
-        trigger: body,
-        start: `${innerHeight} top+=${APP_HEADER_HEIGHT}`,
-        toggleClass: "HAS-REACHED--STEP-2",
-        // markers: true,
+        trigger: "#app-footer",
+        toggleClass: {
+            targets: body,
+            className: "HAS-REACHED--FOOTER",
+        },
+        markers: debug,
     });
 
     // Detect Scroll Direction
@@ -27,16 +49,18 @@ export default function initGsapScrollState() {
             body.classList.toggle("IS-SCROLLING--DOWN", self.direction === 1);
             body.classList.toggle("IS-SCROLLING--UP", self.direction === -1);
         },
-        // markers: true,
+        markers: debug,
     });
 
-    // Detect Footer Visibility
+    // Detect Scroll Progress
     ScrollTrigger.create({
-        trigger: "#app-footer",
-        toggleClass: {
-            targets: body,
-            className: "HAS-REACHED--FOOTER",
+        onUpdate: (self) => {
+            const scrollProgress = self.progress.toFixed(2);
+            document
+                .querySelector("html")
+                .style.setProperty("--SCROLL_PROGRESS", scrollProgress);
+            if (debug) console.log(scrollProgress);
         },
-        // markers: true,
+        markers: debug,
     });
 }
