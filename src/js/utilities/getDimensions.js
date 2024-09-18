58edba19e9d25d41fbb150bf434b8fd1067149fe
Miles <git@itsmil.es>
2024-09-18 21:59:16 +0200

    ++

--- src/js/utilities/getDimensions.js
+++ src/js/utilities/getDimensions.js
@@ -1,5 +1,5 @@
 import { createDebugLogger } from "./debugger";
-const debug = true;
+const debug = false;
 const consoleLog = createDebugLogger(debug);
 
 import toUpperSnakeCase from "./toUpperSnakeCase";
