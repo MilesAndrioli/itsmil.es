62b2e6f9ef6da436b63e7ffba8704dd89e738ca0
Miles <miles.andrioli@gmail.com>
2025-04-26 10:53:18 +0200

    throttled scroll states

---
+++ src/scripts/gsap/initGsapScrollState.js
@@ -0,0 +1,59 @@
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+import throttle from "lodash.throttle";
+
+gsap.registerPlugin(ScrollTrigger);
+
+export default function initGsapScrollState() {
+    const body = document.body;
+
+    // Detect Scroll Milestones
+    ScrollTrigger.create({
+        trigger: body,
+        start: "top top",
+        toggleClass: "HAS-REACHED--STEP-1",
+        markers: false,
+    });
+
+    ScrollTrigger.create({
+        trigger: body,
+        start: `${innerHeight} top`,
+        toggleClass: "HAS-REACHED--STEP-2",
+        markers: false,
+    });
+
+    ScrollTrigger.create({
+        trigger: "#app-footer",
+        toggleClass: {
+            targets: body,
+            className: "HAS-REACHED--FOOTER",
+        },
+        markers: false,
+    });
+
+    // Detect Scroll Direction
+    ScrollTrigger.create({
+        onUpdate: (self) => {
+            body.classList.toggle("SCROLLING--DOWN", self.direction === 1);
+            body.classList.toggle("SCROLLING--UP", self.direction === -1);
+        },
+        markers: false,
+    });
+
+    // Detect Scroll Progress
+    ScrollTrigger.create({
+        onUpdate: throttle(
+            (self) => {
+                const scrollProgress = Math.round(self.progress * 100);
+                document.documentElement.style.setProperty(
+                    "--SCROLL_PROGRESS",
+                    `${scrollProgress}`
+                );
+            },
+            100,
+            { leading: true, trailing: true }
+        ),
+        markers: false,
+    });
+}
