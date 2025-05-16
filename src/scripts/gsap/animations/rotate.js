52f76fa2e5fc9f5e78ee2b0f2ed27040a58cfbb2
Miles <git@itsmil.es>
2025-05-16 02:57:55 +0200

    Refactor Loader component for improved readability, streamline Skeleton layout, add rotation animations, and update sticky item CSS selector for clarity

---
+++ src/scripts/gsap/animations/rotate.js
@@ -0,0 +1,22 @@
+export default {
+    "rotate-45": {
+        origin: { rotate: 0 },
+        destination: { rotate: 45 },
+    },
+    "rotate-90": {
+        origin: { rotate: 0 },
+        destination: { rotate: 90 },
+    },
+    "rotate-135": {
+        origin: { rotate: 0 },
+        destination: { rotate: 135 },
+    },
+    "rotate-180": {
+        origin: { rotate: 0 },
+        destination: { rotate: 180 },
+    },
+    "rotate-360": {
+        origin: { rotate: 0 },
+        destination: { rotate: 360 },
+    },
+};
